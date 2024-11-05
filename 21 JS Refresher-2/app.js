cc5372a92317f0e02f7af385492f09c28775e890
Helal <imhelal.dev@gmail.com>
2024-11-05 23:32:42 +0600

    Day 21 JS Refresher-2

---
+++ 21 JS Refresher-2/app.js
@@ -0,0 +1,63 @@
+// let number1 = 12;
+// let number2 = 11;
+// let isMerrid = false;
+
+// if (isMerrid) {
+//   console.log("Hello world!");
+// }
+
+// switch
+// let number = 1;
+
+// switch ("Dhaka") {
+//   case 1:
+//     console.log("Numbe One");
+//     break;
+//   case 2:
+//     console.log("Numbe Two");
+//     break;
+//   case "Dhaka":
+//     console.log("Numbe Trhee");
+//     break;
+
+//   default:
+//     console.log("No number");
+//     break;
+// }
+
+let userName = "Jhon";
+
+// types of loop
+// for loop
+// foreach loop // dedicated for array
+// while loop
+// do while loop
+// for in loop
+// for of loop
+
+// for loop
+// for (let index = 0; index < 20; index++) {
+//   console.log(userName);
+// }
+
+// while loop
+// let index = 1;
+
+// while (index < 30) {
+//   console.log("Hello");
+//   index++;
+// }
+
+// let dristicts = [
+//   "Dhaka",
+//   "Khulna",
+//   "Barishal",
+//   "Rangpur",
+//   true,
+//   [1, 2, 3, 4],
+//   { name: "test", phone: 342342 },
+// ];
+
+// console.log(dristicts);
+
+let students = ["Jubayer", "Abdullah", "Habib", "Siam"];
