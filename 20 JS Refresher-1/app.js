abf491831f890c18c0df1022b1aacd870b7f0681
Helal <imhelal.dev@gmail.com>
2024-11-05 12:00:08 +0600

    Day 20 JS Refresher

---
+++ 20 JS Refresher-1/app.js
@@ -0,0 +1,40 @@
+// data types in js
+// String
+// Number
+// Boolean
+// null
+// object
+// array
+// undefined
+
+// let greeting = "Hello Bangladesh!";
+// let name = "Jhon";
+// let age = 18;
+// let phone = null;
+// let isMerried = true;
+// let role;
+
+// let students = ["Jubyaer", "abudullah", "siam", 324234];
+
+// let student = {
+//   name: "Jhon",
+//   phone: "4353453",
+//   age: 18,
+// };
+
+// let testname = "Test var";
+
+// operator
+let a = 2;
+let b = 20;
+a += 30;
+
+// if else
+
+let userAge = 23423;
+
+if (userAge < 20) {
+  console.log(true);
+} else {
+  console.log(false);
+}
